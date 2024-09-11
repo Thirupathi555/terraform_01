f10cdabf5e2573b6deb06a25fcf11c1c23397867
Thirupathi555 <thirupathisee339@gmail.com>
2024-09-11 23:45:19 +0530

    ansible

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "terraform" {
 
     ami = var.ami_id # left side and right side names need not to be same
     instance_type = var.instance_type
-    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    vpc_security_group_ids = [aws_security_group.allow_sshh_terraform.id]
     tags = var.tags
 }
 
